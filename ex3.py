a14e56480eace67240b01237bc535702144db2e7
Anupama Suresh <anupamasuresh812@gmail.com>
2024-05-22 13:24:14 +0530

    numbers and math
    - print the string
    - print string along with operations(,+,-,*,/,>,<,%)

---
+++ ex3.py
@@ -0,0 +1,11 @@
+print ("i will count my chickens:")
+
+print("hens",25+30/6)
+
+print("now i will count the eggs")
+
+print(3+2+1-5+4/2-1%4+6)
+
+print ("what is 2+3?",2+3)
+
+print("is it greater",5>-2)
