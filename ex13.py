dd77e5c6770b2124381301dbfaf40cf0e745fb3c
Anupama Suresh <anupamasuresh812@gmail.com>
2024-05-23 17:08:25 +0530

    parameters,unpacking,variables

---
+++ ex13.py
@@ -0,0 +1,11 @@
+# argv is argumenmt variable  holds the arguments passthe python sript
+from sys import argv
+# script refers the  file name and first,second an d third variable name take the arguments in the variables.
+script, first, second, third = argv
+
+print("the script is called:",script)
+print("your first varible is:", first)
+print("your second variable is:", second)
+print("your third variable is:",third)
+
+
