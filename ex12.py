fe1cfe68300f5e634329843d42f9c795994271bb
Anupama Suresh <anupamasuresh812@gmail.com>
2024-05-23 17:03:07 +0530

    prompting people

---
+++ ex12.py
@@ -0,0 +1,6 @@
+#prompting people
+
+age=input("how are you? ")
+height=input("how tall you are? ")
+weight=input("how much do you weight? ")
+print(f"so, you're {age} old, {height} tall and and {weight} heavy.")
