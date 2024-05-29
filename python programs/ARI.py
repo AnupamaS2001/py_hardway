1a5671636d2d0e5f9a2373d683152c5b4f4e10c2
Anupama Suresh <anupamasuresh812@gmail.com>
2024-05-29 10:37:51 +0530

    ARI
    modified the code and fixed the grade kinder garden to 0.

--- python programs/ARI.py
+++ python programs/ARI.py
@@ -16,7 +16,9 @@ print(SENT_COUNT)
 ARI=int(4.71 * (CHAR_COUNT/WORD_COUNT) + 0.5 * (WORD_COUNT/SENT_COUNT) - 21.43)
 print(ARI)
 
-if (ARI==1):
+if (ARI==0):
+    print("Group:Kinder Garden")
+elif (ARI==1):
     print("Group:Kinder Garden")
 elif (ARI==2):
     print("Group:First Grade")
